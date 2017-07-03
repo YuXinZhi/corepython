777cfd1be93a4dfe612fe16cae81fc44c66a2917
Administrator <whoyumengmeng@gmail.com>
2017-07-03 19:55:58 +0800

    字典

--- demostring.py
+++ demostring.py
@@ -36,3 +36,28 @@ print("******************")
 a=(2,'2')
 b=(2,'2')
 print(a==b,id(a),id(b))  #True 31981640 31981896
+
+'''
+    6.20 拷贝Python对象、浅拷贝和深拷贝
+'''
+
+person=["name",["saving",100.00]]
+hubby=person[:]
+wifey=list(person)
+print([id(x) for x in (person,hubby,wifey)])    #[43738760, 43210952, 43739784]
+hubby[0]="joe"
+wifey[0]="jane"
+print(hubby,"\n",wifey)
+print([id(x) for x in hubby])
+print([id(x) for x in wifey])
+hubby[1][1]=50.00
+print(hubby,"\n",wifey)
+print([id(x) for x in hubby])
+print([id(x) for x in wifey])
+'''
+['joe', ['saving', 100.0]] 
+ ['jane', ['saving', 100.0]]
+['joe', ['saving', 50.0]] 
+ ['jane', ['saving', 50.0]]
+
+'''
